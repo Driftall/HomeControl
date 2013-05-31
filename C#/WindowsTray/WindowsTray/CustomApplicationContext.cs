784dcbca3e009dff7163e8f621605e1170cef57f
Driftall <driftall@outlook.com>
2013-06-01 00:40:55 +0100

    Added WindowsTray barebones
    This will replace the original WindowsService idea

---
+++ C#/WindowsTray/WindowsTray/CustomApplicationContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsTray
+{
+    public class CustomApplicationContext : ApplicationContext
+    {
+        //Source: https://www.simple-talk.com/dotnet/.net-framework/creating-tray-applications-in-.net-a-practical-guide/#ninth
+        private static readonly string IconFileName = "home.ico";
+        private static readonly string DefaultTooltip = "Home Control Suite";
+
+        public CustomApplicationContext()
+        {
+            InitializeContext();
+        }
+
+        private System.ComponentModel.IContainer components;	// a list of components to dispose when the context is disposed
+        private NotifyIcon notifyIcon;				            // the icon that sits in the system tray
+
+        private void InitializeContext()
+        {
+            components = new System.ComponentModel.Container();
+            notifyIcon = new NotifyIcon(components)
+            {
+                ContextMenuStrip = new ContextMenuStrip(),
+                Icon = new Icon(IconFileName),
+                Text = DefaultTooltip,
+                Visible = true
+            };
+            //notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
+            //notifyIcon.DoubleClick += notifyIcon_DoubleClick;
+            //notifyIcon.MouseUp += notifyIcon_MouseUp;
+        }
+    }
+}
