3a1cff7f48bfda06ab8a54d44967248b8ff40b64
Driftall <driftall@outlook.com>
2013-06-01 01:42:49 +0100

    Added extra info to GUI

--- Java/PiClient/src/piclient/ApplicationWindow.java
+++ Java/PiClient/src/piclient/ApplicationWindow.java
@@ -9,27 +9,35 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
- *
+ * Source: http://docs.oracle.com/javafx/
  * @author Blake
  */
 public class ApplicationWindow extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        final Label mediaPlaying = new Label("No media is currently playing");
+        final Label time = new Label("01:40 AM");
+        final Label date = new Label("01/06/2013");
         Button btn = new Button();
-        btn.setText("Lights");
+        btn.setText("Media");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                mediaPlaying.setText("HTPC: 2 Fast 2 Furious (5:23/1:24:04)");
                 System.out.println("Lights Toggled");
             }
         });
         
         StackPane root = new StackPane();
+        root.getChildren().add(time);
+        root.getChildren().add(date);
+        root.getChildren().add(mediaPlaying);
         root.getChildren().add(btn);
         
         Scene scene = new Scene(root, 300, 250);
