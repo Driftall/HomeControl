46c86153794f8b6737196701cf25673f2e6ce4a4
Driftall <driftall@outlook.com>
2013-06-14 15:02:04 +0100

    Change PiServer over to default Java Application
    To increase portability

---
+++ Java/PiServer/src/piserver/PiServer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piserver;
+
+/**
+ *
+ * @author Blake
+ */
+public class PiServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Home Control Suite - Pi Server");
+        // TODO code application logic here
+    }
+}
